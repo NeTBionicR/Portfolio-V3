93db56b18e41125c8d8a058d079a2a40684d5225
Adam <e2235970@cmaisonneuve.qc.ca>
2026-02-12 22:35:28 -0500

    Add ripple effect animations and update app structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
 
   return (
-    <div>
+    <div className="app">
+      <div className="circle-ripple" aria-hidden />
+      <div className="circle-ripple circle-ripple--2" aria-hidden />
+      <div className="circle-ripple circle-ripple--3" aria-hidden />
       <h1>Adam Maatouk's Portfolio</h1>
     </div>
   )
